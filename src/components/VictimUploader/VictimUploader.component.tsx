bbcb8a706eab8280a05d739b2c0b7b6bf3dedb42
WDever <alsrbtkfkd7@gmail.com>
2019-06-02 01:15:49 +0900

    Add react-webcam

--- src/components/VictimUploader/VictimUploader.component.tsx
+++ src/components/VictimUploader/VictimUploader.component.tsx
@@ -1,52 +1,19 @@
 import * as React from 'react';
+import Webcam from 'react-webcam';
+
+const { useRef } = React;
 
 interface Props {}
 
-interface State {}
+const VictimUploader: React.FC = () => {
+  const camRef = useRef(null);
 
-class VictimUploader extends React.Component<Props, State> {
-  state: State = {};
-
-  inputRef = React.createRef<HTMLInputElement>();
-
-  componentDidMount() {
-    const inputNode = this.inputRef.current;
-
-    if (inputNode) {
-      inputNode.click();
-    }
-  }
-
-  submitVictimImage = (e: React.FormEvent<HTMLFormElement>): void => {
-    e.preventDefault();
-    // send to photo to the server
-  };
-
-  victimImageChanged = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    // do something here
-  };
-
-  render() {
-    const { submitVictimImage, victimImageChanged, inputRef } = this;
-
-    return (
-      <div>
-        <form onSubmit={submitVictimImage}>
-          <label id="label">
-            Take a picture
-            <br />
-            <input
-              type="file"
-              accept="image/*;capture=camera"
-              onChange={victimImageChanged}
-              ref={inputRef}
-            />
-          </label>
-          <input type="submit" />
-        </form>
-      </div>
-    );
-  }
-}
+  // const getCapture = () => {
+  //   // camRef.current
+  // }
+  return (
+    <Webcam ref={camRef} />
+  );
+};
 
 export default VictimUploader;
