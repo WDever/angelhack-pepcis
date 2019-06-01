b3e037e6af1eaa73cea4f70229dc1e9f38347d3a
WDever <alsrbtkfkd7@gmail.com>
2019-06-02 07:50:49 +0900

    Add Main Component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import './App.scss';
 import VictimUploader from './VictimUploader';
 import VictimFinder from './VictimFinder';
+import MainComponent from './Main';
 
 const App: React.FC = () => (
   <div className="App">
-    <VictimUploader />
+    <Switch>
+      <Route exact path="/register" component={VictimUploader} />
+      <Route exact path="/find" component={VictimFinder} />
+      <Route exact payh="/" component={MainComponent} />
+      <Redirect to="/" />
+    </Switch>
   </div>
 );
 
